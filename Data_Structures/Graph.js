847fdd48b25cbd69b71a77ff005453b3c3ee3d3a
alexbednarek4 <alexbednarek4@gmail.com>
2020-04-23 15:49:29 -0400

    Graph- DFS (recursive and iterative) & BFS (iterative

--- Data_Structures/Graph.js
+++ Data_Structures/Graph.js
@@ -30,15 +30,84 @@ class Graph {
         } 
         delete this.adjacencyList[vertex];
     }
+    // recursive
+    DFSR(start){
+        const result = [];
+        const visited = {};
+        const adjacencyList = this.adjacencyList;
+        //iife
+        (function DFS(vertex){
+            if(!vertex) return null;
+            visited[vertex] = true;
+            result.push(vertex);
+            adjacencyList[vertex].forEach(neighbor => {
+                if(!visited[neighbor]){
+                    return DFS(neighbor);
+                }
+            });
+        })(start)
+        return result;
+    }
+    // iterative
+    DFSI(start){
+        const stack = [start];
+        const visited = {};
+        const result = [];
+        let currentVertex;
+
+        visited[start] = true;
+        while(stack.length){
+            currentVertex = stack.pop();
+            result.push(currentVertex);
+
+            // visit all currentVertex neighbors
+            this.adjacencyList[currentVertex].forEach(neighbor=>{
+                if(!visited[neighbor]){
+                    visited[neighbor] = true;
+                    stack.push(neighbor);
+                }
+            });
+        }
+        return result;
+    }
+    BFS(start){
+        const queue = [start];
+        const result = [];
+        const visited = {};
+        let currentVertex;
+        visited[start] = true;
+
+        while(queue.length){
+            currentVertex = queue.shift();
+            result.push(currentVertex);
+
+            //visit all currentVertex neighbors
+            this.adjacencyList[currentVertex].forEach(neighbor=>{
+                if(!visited[neighbor]){
+                    visited[neighbor] = true;
+                    queue.push(neighbor);
+                }
+            })
+        }
+        return result;
+    }
 }
 const newGraph = new Graph();
 
-newGraph.addVertex('Tokyo');
-newGraph.addVertex('San_Francisco');
-newGraph.addVertex('NYC');
-newGraph.addEdge('Tokyo', 'NYC');
-newGraph.addEdge('NYC', 'San_Francisco');
-// console.log(newGraph.adjacencyList);
-newGraph.removeEdge('Tokyo', 'NYC');
+newGraph.addVertex('A');
+newGraph.addVertex('B');
+newGraph.addVertex('C');
+newGraph.addVertex('D');
+newGraph.addVertex('E');
+newGraph.addVertex('F');
+
+newGraph.addEdge('A', 'B')
+newGraph.addEdge('A', 'C')
+newGraph.addEdge('B', 'D')
+newGraph.addEdge('C', 'E')
+newGraph.addEdge('D', 'E')
+newGraph.addEdge('D', 'F')
+newGraph.addEdge('E','F')
 console.log(newGraph.adjacencyList);
+console.log(newGraph.BFS('A'));
 
