f6ef728b1608fd654d129e22c9dff46076fe23ab
alexbednarek4 <alexbednarek4@gmail.com>
2020-05-03 10:17:37 -0400

    Refactored group anagrams problem

--- LeetCode/groupAnagrams.js
+++ LeetCode/groupAnagrams.js
@@ -13,15 +13,15 @@ Output:
  */
 // Time complexty: O(NWLogN)
 // Must optimize
- function groupAnagrams(strs){
-     let cache = {}
-     for (let str of strs){
-        let sortedKey = str.split('').sort().join('') //aet
-        // if(!cache[sortedKey]){
-        //     cache[sortedkey] = [srt]
-        // }
-        (!cache[sortedKey]) ? cache[sortedKey] = [str] : cache[sortedKey].push(str)
-     }
+function groupAnagrams(strs) {
+  let cache = {};
+  for (let str in strs) {
+    let sortedKey = strs[str].split("").sort().join(""); //aet
+    !cache[sortedKey]
+      ? (cache[sortedKey] = [strs[str]])
+      : cache[sortedKey].push(strs[str]);
+  }
 
-     return Object.values(cache)
- }
+  return Object.values(cache);
+}
+console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
